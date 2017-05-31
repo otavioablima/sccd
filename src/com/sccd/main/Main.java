895d84fdbdf5338bdf5448f32a1ccc59153353a6
otavioablima <otavioablima@gmail.com>
2017-05-31 19:43:51 -0300

    Inclusão de configurações via arquivo properties e ajustes na aplicação

--- src/com/sccd/main/Main.java
+++ src/com/sccd/main/Main.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.PasswordAuthentication;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -20,9 +19,7 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MoveAction;
 
-import com.ibm.watson.developer_cloud.http.ServiceCall;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
@@ -30,57 +27,75 @@ import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
 public class Main {
 
 	public static void main(String[] args) {
-		File dir = new File("C:\\SCCD\\in\\");
+		Properties props = new Properties();
 		
-		SpeechToText service = new SpeechToText();
-		service.setUsernameAndPassword("d8b1177e-85fb-40dd-8be5-b39dca3d2864", "g2nYKtmFaIIn");
-		RecognizeOptions options = new RecognizeOptions.Builder()
-				.contentType("audio/wav")
-				.model("pt-BR_BroadbandModel")
-				.timestamps(true).build();
-		
-		for (File file : dir.listFiles()) {
-			System.out.println("Identificando audio do arquivo " + file.getName());
-			SpeechResults results = service.recognize(file, options).execute();
-			//System.out.println(results);
-			
-			if (results.getResults().get(0).getAlternatives().get(0).getConfidence() >= 0.80) {
-				String audioTranscription = results.getResults().get(0).getAlternatives().get(0).getTranscript();
-				System.out.println("TRANSCRICAO DO AUDIO: " + audioTranscription);
-				sendMail(audioTranscription);
-				moveFile(file, "C:\\SCCD\\out\\");
-			} else {
-				System.out.println("Não foi possível identificar a fala do áudio.");
-				moveFile(file, "C:\\SCCD\\error\\");
-			}
-		}
+        try{
+        	props = new Properties();
+            InputStream in = new FileInputStream(new File(ClassLoader.getSystemResource("").getPath() + "properties.properties"));
+        	props.load(in);
+        	in.close();
+        	
+        	File dir = new File(props.getProperty("in.dir"));
+    		
+    		SpeechToText service = new SpeechToText();
+    		service.setUsernameAndPassword(props.getProperty("watson.speech2text.username"), props.getProperty("watson.speech2text.password"));
+    		RecognizeOptions options = new RecognizeOptions.Builder()
+    				.contentType("audio/wav")
+    				.model("pt-BR_NarrowbandModel")
+    				.timestamps(true).build();
+    		
+    		for (File file : dir.listFiles()) {
+    			System.out.println("Identificando audio do arquivo " + file.getName());
+    			SpeechResults results = service.recognize(file, options).execute();
+    			//System.out.println(results);
+    			String audioTranscription = results.getResults().get(0).getAlternatives().get(0).getTranscript();
+    			
+    			if (results.getResults().get(0).getAlternatives().get(0).getConfidence() >= 0.60) {
+    				System.out.println("E-mail enviado.");
+    				System.out.println("Transcrição do áudio: " + audioTranscription);
+    				//sendMail(audioTranscription);
+    				sendMail(props.getProperty("mail.server.sender"),
+    						props.getProperty("mail.server.receiver"),
+    						"Abertura de chamado via URA",
+    						audioTranscription,
+    						props.getProperty("mail.server.host"),
+    						props.getProperty("mail.server.port"));
+    				moveFile(file, props.getProperty("out.dir"));
+    			} else {
+    				System.out.println("E-mail não enviado devido grau de confiança do reconhecimento estar abaixo de 60%.");
+    				System.out.println("Transcrição do áudio: " + audioTranscription);
+    				moveFile(file, props.getProperty("error.dir"));
+    			}
+    		}
+    		
+        } catch (FileNotFoundException e) {
+        	e.printStackTrace();
+		} catch (IOException e) {
+        	e.printStackTrace();
+        }
 	}
 	
-	private static void sendMail(String desc) {
+	private static void sendMail(String from, String to, String subject, String desc, String mailServer, String mailPort) {
 		Properties props = new Properties();
         
-        props.put("mail.smtp.host", "192.168.2.19");
-        //props.put("mail.smtp.socketFactory.port", "465");
-        //props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        //props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "25"); //port 25 or 2525 or 587 - Secure SMTP (SSL / TLS) - port 465 or 25 or 587, 2526 (Elastic Email)
+        props.put("mail.smtp.host", mailServer);
+        props.put("mail.smtp.port", mailPort); //port 25 or 2525 or 587 - Secure SMTP (SSL / TLS) - port 465 or 25 or 587, 2526 (Elastic Email)
 
         Session session = Session.getDefaultInstance(props, null);
         
-        session.setDebug(true);
+        session.setDebug(false);
         
         try {
 
 			Message message = new MimeMessage(session);
 
-			Address[] toUser = InternetAddress.parse("msollitari@magnasistemas.com.br"); 
-			message.setFrom(new InternetAddress("olima@magnasistemas.com.br"));
+			Address[] toUser = InternetAddress.parse(to); 
+			message.setFrom(new InternetAddress(from));
 			message.setRecipients(Message.RecipientType.TO, toUser);
-			message.setSubject("Chamado");
+			message.setSubject(subject);
 			message.setText(desc);
 			
 			Transport.send(message);
-			System.out.println("Feito!!!");
 			
         } catch (MessagingException e) {
         	throw new RuntimeException(e);
